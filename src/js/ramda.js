c53a3b3f622eaad3d41aee5b159950a1fbec6bb2
Jaemok <ashnamuh@gmail.com>
2019-07-22 13:51:02 +0900

    add checkValidators func

--- src/js/ramda.js
+++ src/js/ramda.js
@@ -1,4 +1,21 @@
-import { pipe, find } from 'ramda' // too big. Not splitted
+const validators = [
+  val => typeof val === 'string' || 'No number',
+  val => val.length > 1 || 'minimum 2 length',
+  val => val.length <= 5 || 'maximum 5 length'
+]
 
-import pipe from 'ramda/src/pipe' // splitted
-import find from 'ramda/src/find' // splitted
+function checkValidators(fs, val) {
+  const [validator, ...rest] = fs
+  const fResult = validator(val)
+  if (fResult === true && rest.length !== 0) {
+    return checkValidators(rest, val)
+  }
+  if (fResult === true) {
+    return { isValid: fResult, validationMessage: null }
+  }
+  return { isValid: false, validationMessage: fResult }
+}
+
+const result = checkValidators(validators, 'input')
+
+console.log(result)
